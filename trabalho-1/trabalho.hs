770a1cc96ca78b42cb501110e797337cc06ec53b
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-09-02 20:21:29 -0300

    Correção da Primeira Questão

--- trabalho-1/trabalho.hs
+++ trabalho-1/trabalho.hs
@@ -8,7 +8,7 @@
 
 triangulo :: (Float, Float, Float) -> String
 triangulo (a, b, c)
-    | (a + b + c) /= 180 = "Nao e triangulo"
+    | ((a + b + c) /= 180) || a <= 0 || b <= 0 || c <= 0 = "Nao e triangulo"
     | (a == b) && (b == c) = "Equilatero"
     | (a == 90) || (b == 90) || (c == 90) = "Retangulo"
     | (a > 90) || (b > 90) || (c > 90) = "Obtuso"
@@ -22,3 +22,8 @@ equacao :: Float -> Float -> Float -> (Float, Float)
 equacao a b c
     | a /= 0 = ((-b + sqrt(b^2 - 4 * a * c))/(2 * a), (-b - sqrt(b^2 - 4 * a * c))/(2 * a))
     | otherwise = ((-c)/b , a)
+
+{-
+    Questão 3
+-}
+
